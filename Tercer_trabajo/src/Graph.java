d036b6bcb2e07b11794fad306d5f8b8e1ff13b41
ImanolMM <imanolm.upv@gmail.com>
2022-11-12 11:48:24 +0100

    Clase Graph

---
+++ Tercer_trabajo/src/Graph.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Graph {
+
+    public boolean estanConectados(String a1, String a2){
+        Queue<Integer> porExaminar = new LinkedList<Integer>();
+
+        int pos1 ;
+        int pos2 ;
+        boolean enc = false;
+        boolean[] examinados = new boolean[2];
+
+        // COMPLETAR CÓDIGO
+
+        return enc;
+
+    }
+}

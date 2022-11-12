5b365fadba1d985cd92db4b0f877a2884c665fb3
ImanolMM <imanolm.upv@gmail.com>
2022-11-12 11:57:40 +0100

    alsdf

--- Tercer_trabajo/src/Graph.java
+++ Tercer_trabajo/src/Graph.java
@@ -1,9 +1,11 @@
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Graph {
 
-    public boolean estanConectados(String a1, String a2){
+    public boolean estanConectados(String a1, String a2) throws IOException {
+        Main.cargarWebs("a");
         Queue<Integer> porExaminar = new LinkedList<Integer>();
 
         int pos1 ;
